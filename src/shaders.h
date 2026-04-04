030535293b4c21d92ccaeada93583dac290e711f
Rob Loach <robloach@gmail.com>
2026-04-04 11:44:41 -0400

    Fix shader

--- src/shaders.h
+++ src/shaders.h
@@ -1,5 +1,6 @@
-#ifndef RAYLIB_LIBRETRO_SHADERS
-#define RAYLIB_LIBRETRO_SHADERS
+#ifndef RAYLIB_LIBRETRO_SHADERS_H
+#define RAYLIB_LIBRETRO_SHADERS_H
+
 #include "raylib.h"
 
 #if defined(PLATFORM_DESKTOP)
@@ -18,10 +19,10 @@ typedef struct ShaderCRT {
     float border;
 } ShaderCRT;
 
-#define MAX_SHADERS 2
-Shader shaders[MAX_SHADERS];
-ShaderCRT shaderCRT;
-int currentShader = 0;
+#define RAYLIB_LIBRETRO_SHADERS_MAX 2
+static Shader shaders[RAYLIB_LIBRETRO_SHADERS_MAX];
+static ShaderCRT shaderCRT;
+static int currentShader = 0;
 
 /**
  * Loads the scanline shader code at compile time.
@@ -48,6 +49,8 @@ const char* GetShaderCodeCRT() {
     const char* code =
 #if GLSL_VERSION == 100
 #include "shaders/crt-glsl100.txt"
+#elif GLSL_VERSION == 120
+#include "shaders/crt-glsl120.txt"
 #elif GLSL_VERSION == 330
 #include "shaders/crt-glsl330.txt"
 #else
@@ -59,20 +62,20 @@ const char* GetShaderCodeCRT() {
 
 Shader LoadShaderCRT() {
     Shader shader = LoadShaderFromMemory(NULL, GetShaderCodeCRT());
-    float brightnessLoc = GetShaderLocation(shader, "Brightness");
-    float ScanlineIntensityLoc = GetShaderLocation(shader, "ScanlineIntensity");
-    float curvatureRadiusLoc = GetShaderLocation(shader, "CurvatureRadius");
-    float cornerSizeLoc = GetShaderLocation(shader, "CornerSize");
-    float cornersmoothLoc = GetShaderLocation(shader, "Cornersmooth");
-    float curvatureLoc = GetShaderLocation(shader, "Curvature");
-    float borderLoc = GetShaderLocation(shader, "Border");
+    int brightnessLoc = GetShaderLocation(shader, "Brightness");
+    int ScanlineIntensityLoc = GetShaderLocation(shader, "ScanlineIntensity");
+    int curvatureRadiusLoc = GetShaderLocation(shader, "CurvatureRadius");
+    int cornerSizeLoc = GetShaderLocation(shader, "CornerSize");
+    int cornersmoothLoc = GetShaderLocation(shader, "Cornersmooth");
+    int curvatureLoc = GetShaderLocation(shader, "Curvature");
+    int borderLoc = GetShaderLocation(shader, "Border");
     shaderCRT.brightness = 1.0f;
     shaderCRT.scanlineIntensity = 0.5f;
     shaderCRT.curvatureRadius = 0.4f;
     shaderCRT.cornerSize = 5.0f;
     shaderCRT.cornersmooth = 35.0f;
-    shaderCRT.curvature = true;
-    shaderCRT.border = true;
+    shaderCRT.curvature = 1.0f;
+    shaderCRT.border = 1.0f;
     SetShaderValue(shader, GetShaderLocation(shader, "resolution"), &((Vector2){GetScreenWidth(), GetScreenHeight()}), SHADER_UNIFORM_VEC2);
     SetShaderValue(shader, brightnessLoc, &shaderCRT.brightness, SHADER_UNIFORM_FLOAT);
     SetShaderValue(shader, ScanlineIntensityLoc, &shaderCRT.scanlineIntensity, SHADER_UNIFORM_FLOAT);
@@ -87,14 +90,14 @@ Shader LoadShaderCRT() {
 void UpdateShaders() {
     // Check to see if we are to change the shader.
     if (IsKeyPressed(KEY_F10)) {
-        if (++currentShader > MAX_SHADERS) {
+        if (++currentShader > RAYLIB_LIBRETRO_SHADERS_MAX) {
             currentShader = 0;
         }
     }
 
     // Update resolution of CRT shader.
     if (IsWindowResized()) {
-        SetShaderValue(shaders[0], GetShaderLocation(shaders[0], "resolution"), &((Vector2){GetScreenWidth(), GetScreenHeight()}), 1);
+        SetShaderValue(shaders[0], GetShaderLocation(shaders[0], "resolution"), &((Vector2){GetScreenWidth(), GetScreenHeight()}), SHADER_UNIFORM_VEC2);
     }
 }
 
@@ -104,7 +107,7 @@ void LoadShaders() {
 }
 
 void UnloadShaders() {
-    for (int i = 0; i < MAX_SHADERS; i++) {
+    for (int i = 0; i < RAYLIB_LIBRETRO_SHADERS_MAX; i++) {
         UnloadShader(shaders[i]);
     }
 }
--- src/shaders/README.md
+++ src/shaders/README.md
@@ -1,5 +1,11 @@
-# Shader C Code
+# Shaders
 
-The provided .txt files is the shader code escaped for C code.
+## CRT Shader
+
+Anata on raylib Discord.
+
+## TXT Escaper
+
+The .txt files is the shader code escaped for C code:
 
 https://tomeko.net/online_tools/cpp_text_escape.php
